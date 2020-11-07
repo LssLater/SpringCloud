b600d134750e4cf6b11d75ef27c2578f7e116539
yuyang <528952805@qq.com>
2020-11-08 00:28:07 +0800

    SpringCloud--hystrix

---
+++ cloud-gateway-gateway9527/src/main/java/com/yu/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.yu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: yy
+ * @Date: 2020/11/7 14:41
+ * @Version: 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
