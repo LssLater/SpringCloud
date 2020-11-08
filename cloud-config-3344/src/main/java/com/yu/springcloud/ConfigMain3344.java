02aeedd7c6b5a84ea77ee3850d241efdc4e6985a
yuyang <528952805@qq.com>
2020-11-08 18:57:34 +0800

    SpringCloud--config

---
+++ cloud-config-3344/src/main/java/com/yu/springcloud/ConfigMain3344.java
@@ -0,0 +1,18 @@
+package com.yu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: yy
+ * @Date: 2020/11/8 14:43
+ * @Version: 1.0.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/yu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.yu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: yy
+ * @Date: 2020/11/8 15:25
+ * @Version: 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
